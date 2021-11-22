eccd4228ea40009555a12ec0662d00684817b9e4
Sagar Chavan <sagarchavan6210@gmail.com>
2021-11-22 13:36:01 +0530

    Create addition_of_2_numbers.py

---
+++ Python/addition_of_2_numbers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+num1 = input('Enter 1st number  : ')
+num2 = input('Enter 2nd number  : ')
+#num1 = 1.5
+#num2 = 6.3
+
+sum = float(num1) + float(num2) # for integer sum = int(num1) + int(num2)
+
+print ("addition of {0}  and {1} is : {2}".format(num1, num2, sum))
